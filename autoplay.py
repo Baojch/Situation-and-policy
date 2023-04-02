48823c0e29172bccb6b62796581b1268fc17e936
baojiacheng <baojch@shanghaitech.edu.cn>
2023-04-02 09:01:42 +0800

    update

--- autoplay.py
+++ autoplay.py
@@ -2,15 +2,41 @@ import time
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.common.action_chains import ActionChains
+ 
 USERNAME = "15201784147"
 PASSWORD = "baojiacheng1998"
+# start from which chapter and lecture
+chapter = 4
+lec = 6
 
 chromedriver = "/Users/bjc/Desktop/skd/形势与政策/chromedriver"
 web = Chrome(chromedriver)
-
+web.set_window_size(1600, 1300) 
 web.get('http://passport2.chaoxing.com/login?loginType=4&newversion=true&fid=10572&refer=http://i.mooc.chaoxing.com')
 
+# functions
+def judging_prob():
+    web.find_element(By.CLASS_NAME, 'ans-videoquiz-opt').click()
+    web.find_element(By.LINK_TEXT,'提交').click()
+    if(pause_btn.get_attribute('title') == "播放"):
+        print("ans = 是")
+        return
+    else:
+        web.find_element(By.CLASS_NAME, 'ans-videoquiz-opt')[3].click()
+        web.find_element(By.LINK_TEXT,'提交').click()
+        print("ans = 否")
+        return
+def judging_existence():
+    try:
+        #因发现有时播放完毕不显示重播按钮，故加入判断
+        #判断是否有居中播放元素，有则表示播放完毕
+        if(pause_btn.get_attribute('title') == "重播"):
+            print("本节播放完毕")
+            return True
+        return False
+    except:
+        return False
 # 1. 登录
 phone = web.find_element(By.CLASS_NAME,'ipt-tel')
 pwd = web.find_element(By.CLASS_NAME,'ipt-pwd')
@@ -40,7 +66,7 @@ web.switch_to.frame(frame_content)
 time.sleep(4)
 
 #起始页面
-begin_class = web.find_element(By.XPATH,'//div[@class="chapter_td"]/div[2]/div[2]/ul/li[1]/div')
+begin_class = web.find_element(By.XPATH,'//div[@class="chapter_td"]/div['+str(chapter+1)+']/div[2]/ul/li['+str(lec)+']/div')
 #第二个div【章节数+1】，li【课程数】
 # 跳转到第一个知识点页面
 begin_class.click()
@@ -63,22 +89,26 @@ print("开始播放")
 time.sleep(10)
 # 播放和暂停按钮
 pause_btn = web.find_element(By.XPATH,'//div[@class="vjs-control-bar"]/button')
-
+js="var q=document.documentElement.scrollTop=10000"  # 滚动到最下面
+web.execute_script(js)
+    
 while (1):  # 播放等待
     try:
-        time.sleep(5)  # 每1秒，检查视频是否播放完毕
+        time.sleep(2)  # 每2秒，检查视频是否播放完毕
         if (pause_btn.get_attribute('title') == "播放"):  #暂停时播放
             pause_btn.click()
             print("继续播放")
-        if (pause_btn.get_attribute('title') == "重播"):  # 点击后播放，即播放完毕状态
+    
+        if (judging_existence()):  # 点击后播放，即播放完毕状态
             print('视频播放完毕')
             web.refresh()
-            web.maximize_window()
             time.sleep(5)
-            web.find_element(By.XPATH, '//div[@class="jb_btn jb_btn_92 fs14 prev_next next"]').click()
+            web.find_element(By.XPATH, '//div[@class="jb_btn jb_btn_92 fs14 prev_next next fr"]').click()
             print("进入章节测验")
-            time.sleep(8)
-            web.find_element(By.XPATH, '//*[@class="jb_btn jb_btn_92 fs14 prev_next next"]').click()
+            time.sleep(5)
+            js="var q=document.documentElement.scrollTop=10000"  # 滚动到最下面
+            web.execute_script(js)
+            web.find_element(By.XPATH, '//*[@class="jb_btn jb_btn_92 fs14 prev_next next fr"]').click()
             time.sleep(3)
             web.find_element(By.XPATH, '//*[@class="jb_btn jb_btn_92 fr fs14 nextChapter"]').click()
             print("进入下一章")
@@ -93,11 +123,12 @@ while (1):  # 播放等待
             time.sleep(10)
             # 播放和暂停按钮
             pause_btn = web.find_element(By.XPATH, '//div[@class="vjs-control-bar"]/button')
-    except:
+            js="var q=document.documentElement.scrollTop=10000"  # 滚动到最下面
+            web.execute_script(js)
 
+    except:
         print("出现问题")
-        web.find_element(By.CLASS_NAME, 'ans-videoquiz-opt').click()
-        web.find_element(By.LINK_TEXT,'提交').click()
+        judging_prob()
 
 
 
